6c6cf487f6e1d8530fd9f97f12db2d4b39c49033
jpansuwan05 <j.pansuwan05@gmail.com>
2026-09-20 01:25:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -274,8 +274,8 @@ if st.button("📊 คำนวณและส่งออกไฟล์ Excel 
         output = io.BytesIO()
         try:
             wb = openpyxl.load_workbook("template_51.xlsx")
-            ws_work_template = wb["แบบค่าทำงาน"]  
-            ws_holiday_template = wb["แบบวันหยุด"] 
+            ws_work_template = wb["ค่าทำงาน"]  
+            ws_holiday_template = wb["วันหยุด"] 
 
             def replace_tags_in_sheet(ws, page_num):
                 replacements = {
